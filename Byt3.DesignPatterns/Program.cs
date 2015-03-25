8df0baacd8e083b73c8300e8e4e22f51e68dd164
mr-ndrw <andrew.torski@gmail.com>
2015-03-25 11:39:48 +0100

    init

---
+++ Byt3.DesignPatterns/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Byt3.DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
